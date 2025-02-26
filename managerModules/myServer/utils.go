17e5d03face32efff85a596fc25f48da2cfbbc02
jooyumm <kwonjy2003@gmail.com>
2025-02-26 15:20:33 +0900

    code3

--- managerModules/myServer/utils.go
+++ managerModules/myServer/utils.go
@@ -17,3 +17,23 @@ func check(argErr error) {
 		log.Printf("Error: %v", argErr)
 	}
 }
+
+func SEND_REST_DATA(argAddr string, argJsonData interface{}) (*http.Response, string) {
+
+	// JSON으로 변환
+	jsonData, err := json.Marshal(&argJsonData)
+	check(err)
+
+	// 변환된 JSON 데이터를 로그에 출력
+	log.Printf("Sending POST request to %s with the following JSON data:\n%s", argAddr, string(jsonData))
+
+	// POST 요청에서 Content-Type을 application/json으로 설정
+	resp, err := http.Post(argAddr, "application/json", bytes.NewBuffer(jsonData))
+	check(err)
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	check(err)
+
+	return resp, string(body)
+}
