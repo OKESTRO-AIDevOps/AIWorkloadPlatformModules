dcd27caaed59ddf7011315a59ea481a9d71ebfc4
unknown <db021129@naver.com>
2025-02-27 10:51:28 +0900

    add code2

--- rsModules/function/network_stress_module.py
+++ rsModules/function/network_stress_module.py
@@ -1,2 +1,12 @@
 import time
 import requests
+
+def send_large_post_request(network_mode, net_url, net_port):
+    # Create large data
+    if network_mode=="preprocess":
+        large_data = 'x' * 10**8  # 10MB data
+    else:
+        large_data = 'x' * 10**4
+    url = f"{net_url}:{net_port}/post"
+    response = requests.post(url, data=large_data)
+    print(f"Sent {len(large_data)} bytes to {url}, received {len(response.content)} bytes")
